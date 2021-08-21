8ff9f8f9b1c4b37a68ea95445a4a54ffd028507a
Samuel Caitano <smlcaitano@gmail.com>
2021-08-20 23:22:27 -0300

    new commit

---
+++ POO C#/unit I/ex01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string username;
+            string password;
+            int cont = 0;
+            int dd = 0;
+
+            System.Console.WriteLine("-------------------------");
+            System.Console.WriteLine("usuário: admin \nsenha: admin");
+            System.Console.WriteLine("-------------------------");
+
+            /*  executa enquanto os dados inseridos forem incorretos ou ate atingir o limite de tentativas */
+            do
+            {
+                System.Console.WriteLine("Insira o nome do usuário: ");
+                username = Console.ReadLine();
+
+                System.Console.WriteLine("Insira a senha: ");
+                password = Console.ReadLine();
+                System.Console.WriteLine("-------------------------");
+
+                // verificar se os dados inseridos estão corretos
+                if (username == "admin" && password == "admin")
+                {
+                    dd = 1;
+                    cont = 3;
+                }
+                else
+                {
+                    dd = 0;
+                    cont++;
+                }
+            } while ((username != "username" || password != "password") && (cont != 3));
+
+            if (dd == 1)
+                System.Console.WriteLine("Senha correta!");
+            else if (dd == 0)
+                System.Console.WriteLine("Você excedeu as três tentativas de login!");
+            
+            Console.ReadLine();
+        }        
+    }
+}
