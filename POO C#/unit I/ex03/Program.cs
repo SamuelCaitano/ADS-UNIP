90232ad86f07cfaee3c8a30d60480fe1f8aa665e
Samuel Caitano <smlcaitano@gmail.com>
2021-08-21 00:06:33 -0300

    new commit

---
+++ POO C#/unit I/ex03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ex02
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num1;
+
+            System.Console.WriteLine("Digite um número inteiro");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 % 2 == 0)
+                System.Console.WriteLine("{0} é um número par.", num1);
+            else
+                System.Console.WriteLine("{0} é um número impar.", num1);
+            
+            Console.Read();
+        }
+    }
+}
---
+++ POO C#/unit I/ex03/README.md
@@ -0,0 +1,3 @@
+# Lista 1 - Programação Orientada a Objetos II - ADS-UNIP
+ 
+## Exercício (3.) Escreva um programa em C# que verifique se um número dado é par ou ímpar. Entrada para teste: 15 Saída esperada: 15 é um número ímpar.
