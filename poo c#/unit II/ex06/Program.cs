a597396dbbc558976c09658a91e014b8a5298184
Samuel Caitano <smlcaitano@gmail.com>
2021-08-24 16:26:18 -0300

    new commit

---
+++ poo c#/unit II/ex06/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            int num;
+            int soma = 0;
+
+            Console.WriteLine("\nDigite um número natural:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Os primeiros {0} números naturais são:", num);
+            for (i = 1; i <= num; i++)
+            {
+                Console.Write("{0}", i);
+                if (i < num)
+                    Console.Write("x");
+
+                soma += i;
+            }
+            Console.WriteLine("\nA soma dos números naturais até o {0}, temos: {1}", num, soma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ poo c#/unit II/ex06/README.md
@@ -0,0 +1,3 @@
+# Lista 1 - Programação Orientada a Objetos II - ADS-UNIP
+ 
+## Exercício (6.) Escreva um programa em C# para exibir <em>n</em> termos de um número natural e a soma dos mesmos.
