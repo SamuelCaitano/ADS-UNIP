1acb48222ac6e7d548b121acde0269ee1a8f89ed
Samuel Caitano <smlcaitano@gmail.com>
2021-08-25 00:38:57 -0300

    new commit

---
+++ poo c#/unit II/ex08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ex08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[100];
+            int i;
+            int num;
+            int soma = 0;
+
+            Console.WriteLine("Digite o número de elementos que devem ser adcionados no array: ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Inserie {0} elementos no array:\n", num);
+
+            // percorre o vetor e adciona a entrada do teclado
+            for (i = 0; i < num; i++)
+            {
+                Console.WriteLine("elemento-{0}", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());            
+            }
+
+            // faz a soma dos valores
+            for (i = 0; i < num; i++)
+                soma+= a[i];
+            
+            Console.WriteLine("A soma de todos os elementos do array é: {0}", soma);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ poo c#/unit II/ex08/README.md
@@ -0,0 +1,3 @@
+# Lista 1 - Programação Orientada a Objetos II - ADS-UNIP
+ 
+## Exercício (8.) Escreva um programa em C# que calcule a soma de todos elementos de um array.
